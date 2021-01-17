7db63255111b3dedb87720d6e96d0b2c7efbfc36
mart-j <m.jukss@gmail.com>
2021-01-17 21:02:58 +0200

    added content data for preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React, { FC } from 'react';
+import data from './data/index'
 import './App.css';
 import Carousel from './components/carousel/Carousel';
 
 const App: FC = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
   return (
-    <Carousel>
-      {arr.map((item, i) => {
-        return (
-          <div className="wrapper" key={`${i}`}>
-            <h1>{i}</h1>
-            <h3>{i + 3}</h3>
-          </div>
-        );
-      })}
-    </Carousel>
+    <>
+    <h1 className="heading">Reusable Slider Component for any HTML content</h1>
+      <Carousel>
+        {data.map(({ description, title, button }, i) => {
+          return (
+            <div className="wrapper" key={`${i}`}>
+              <h1>{title}</h1>
+              <h3>{description}</h3>
+              <div className="buttonWrapper">
+                <button className="button">{button}</button>
+              </div>
+            </div>
+          );
+        })}
+      </Carousel>
+    </>
   );
 };
 export default App;
---
+++ src/data/index.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: ' Animated Book Star Rating in React Native',
+    description:
+      'Collection component contains the validation function which validates the form state based on the text length of the editor and also a reducer',
+    button: 'read more',
+  },
+  {
+    title: 'Styling vs Scripting. Which one is the hardest to learn/control?',
+    description:
+      "If we wanted to build something similar to its what's happening bar, we could think to develop it as a web Form.",
+    button: 'read more',
+  },
+  {
+    title: "Bulding a Twitter What's Happening Bar Form in Reactjs",
+    description:
+      'When we post a new Tweet it basically consists of a collection of information like images, videos, plain text, emoji.',
+    button: 'read more',
+  },
+];
